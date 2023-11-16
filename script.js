7955b5103f0ff4d7d6c660449a64659c45fe72ab
piyush <piyushbhatt053@gmail.com>
2023-11-16 11:06:46 +0530

    changes

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-var tl = gsap.timeline();
-
-
-tl.to("#page1",{
-    y:"100vh",
-    scale:0.6,
-    duration:0
-})
-tl.to("#page1",{
-    y:"30vh",
-    duration:1,
-    delay:1
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true,
 })
 
-tl.to("#page1",{
-    y:"0vh",
-    rotate:360,
-    scale:1,
-    duraton:0.8
+var tl = gsap.timeline()
 
+tl.to('#page1', {
+  y: '100vh',
+  scale: 0.6,
+  duration: 0,
+})
+tl.to('#page1', {
+  y: '30vh',
+  duration: 1,
+  delay: 1,
+})
+
+tl.to('#page1', {
+  y: '0vh',
+  rotate: 360,
+  scale: 1,
+  duraton: 0.8,
 })
